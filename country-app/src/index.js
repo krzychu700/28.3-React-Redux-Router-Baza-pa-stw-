34a291bef3baed8f048c5fe0c75a273db022bd62
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-15 22:11:09 +0100

    task 2 finished

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import routes from './routes';
 import { getCountries } from './actions/actions-countries';
 import DevTools from './DevTools';
 
 render(
-    <Provider store={store}>
+  <Provider store={store}>
       <div>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools />
+      <Router history={hashHistory} routes={routes}/>
+      <DevTools />
       </div>
-    </Provider>,
-    document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
 
 store.dispatch(getCountries());
+
