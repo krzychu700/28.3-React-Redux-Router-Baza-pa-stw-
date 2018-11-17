4b9cbbc29c89b6b8e1aa421be43f1d891b34dbe4
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-17 20:11:30 +0100

    fixed errors, task finished

--- country-app/src/presentational/navigation.component.js
+++ country-app/src/presentational/navigation.component.js
@@ -5,24 +5,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="/">Państwa.js</Link>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries">Countries</Link></li>
-                        <li><Link to="/continents">Continents</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            {props.children}
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Państwa.js</Link>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link className="nav-link" to="/countries">Countries</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/continents">Continents</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <div className="container-fluid">
+        {props.children}
+      </div>
     </div>
-);
-
-export default Navigation;
+  );
+  
+  export default Navigation;
