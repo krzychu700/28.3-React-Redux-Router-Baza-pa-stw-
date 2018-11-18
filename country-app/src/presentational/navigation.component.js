6ca5b11d9c5a26ec564074a0273b089de7d9d72b
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-18 19:44:01 +0100

    fixed navigation

--- country-app/src/presentational/navigation.component.js
+++ country-app/src/presentational/navigation.component.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap-theme.css';  ---- w mojej zainstalowanej wersji bootstrapa tego nie ma
 
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="/">Państwa.js</Link>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries">Countries</Link></li>
-                        <li><Link to="/continents">Continents</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-            {props.children}
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Państwa.js</Link>
+          </div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link className="nav-link" to="/countries">Countries</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/continents">Continents</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <div className="container-fluid">
+        {props.children}
+      </div>
     </div>
-);
-
-export default Navigation;
+  );
+  
+  export default Navigation;
