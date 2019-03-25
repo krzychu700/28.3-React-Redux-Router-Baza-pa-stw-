49023c8314a7c7b384c555d07e187751b328c2ff
Krzysztof Szypulski <krzychu700@gmail.com>
2019-03-25 19:47:41 +0100

    Updates

---
+++ precache-manifest.9a906f89fd4cce3ed8eb42df86606eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea8cc360b3b8992a6b54",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/static/js/runtime~main.ea8cc360.js"
+  },
+  {
+    "revision": "a0fa249e94516611cb71",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/static/js/main.a0fa249e.chunk.js"
+  },
+  {
+    "revision": "5328dbd0ba25a832f713",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/static/js/1.5328dbd0.chunk.js"
+  },
+  {
+    "revision": "a0fa249e94516611cb71",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/static/css/main.81a474d9.chunk.css"
+  },
+  {
+    "revision": "5328dbd0ba25a832f713",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/static/css/1.10caee68.chunk.css"
+  },
+  {
+    "revision": "e0366c7ba7ffff29bc9e5601a1dd95ef",
+    "url": "/28.3-React-Redux-Router-Baza-panstw/index.html"
+  }
+];
